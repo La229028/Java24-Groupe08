2d23e930ecb9d156d20403409855f4124994f381
lamrij <la227479@student.helha.be>
2024-04-03 09:58:38 +0200

    suppression du mauvais TODO

--- src/main/java/helha/java24groupe08/controllers/IndexApplication.java
+++ src/main/java/helha/java24groupe08/controllers/IndexApplication.java
@@ -1,6 +1,6 @@
-package com.java24groupe08.controllers;
+package helha.java24groupe08.controllers;
 
-import com.java24groupe08.views.IndexViewController;
+import helha.java24groupe08.views.IndexViewController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
