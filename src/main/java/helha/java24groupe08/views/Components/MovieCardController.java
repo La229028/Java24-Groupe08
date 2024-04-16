f506d61e7efac4ed043acdfa61e3101e11206896
Valentin <la229028@student.helha.be>
2024-04-16 18:14:16 +0200

    Ajout du dossier Components + class MovieCard

---
+++ src/main/java/helha/java24groupe08/views/Components/MovieCardController.java
@@ -0,0 +1,33 @@
+package helha.java24groupe08.views.Components;
+
+import javafx.fxml.FXML;
+import javafx.scene.layout.VBox;
+
+import javax.swing.text.html.ImageView;
+import java.awt.*;
+import java.util.List;
+
+public class MovieCardController {
+
+    @FXML
+    private VBox movieCard;
+    @FXML
+    private Label titleLabel;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Button seeMoreButton;
+
+
+    public void createVBox(String[] movie) {
+        titleLabel.setText(movie[0]);
+        Image image = new Image(movie[1]);
+        imageView.setImage(image);
+        seeMoreButton.setOnAction(event -> seeMoreButtonAction(movie));
+    }
+
+
+
+
+
+}
