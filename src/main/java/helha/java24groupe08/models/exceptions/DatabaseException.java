5bc1d3c8ff9e48405b49b433a8ef2cb276767a0c
Lyara <la227988@student.helha.be>
2024-05-11 10:59:39 +0200

    workspace

---
+++ src/main/java/helha/java24groupe08/models/exceptions/DatabaseException.java
@@ -0,0 +1,7 @@
+package helha.java24groupe08.models.exceptions;
+
+public class DatabaseException extends Exception{
+    public DatabaseException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
